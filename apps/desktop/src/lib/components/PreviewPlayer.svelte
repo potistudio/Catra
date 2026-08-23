0d56d41b4412cffd073ec59fa01325297a38eef7
potistudio <potistudio@proton.me>
2026-08-24 07:39:53 +0900

    restore clockwise volume slider direction

--- apps/desktop/src/lib/components/PreviewPlayer.svelte
+++ apps/desktop/src/lib/components/PreviewPlayer.svelte
@@ -401,12 +401,12 @@ onDestroy(() => {
           viewBox="0 0 56 56"
           aria-hidden="true"
         >
-          <path class="knob-track" pathLength="100" d="M44.97 44.97A24 24 0 1 0 11.03 44.97"></path>
+          <path class="knob-track" pathLength="100" d="M11.03 44.97A24 24 0 1 1 44.97 44.97"></path>
           <path
             class="knob-level"
             pathLength="100"
             stroke-dasharray={`${volume * 100} 100`}
-            d="M44.97 44.97A24 24 0 1 0 11.03 44.97"
+            d="M11.03 44.97A24 24 0 1 1 44.97 44.97"
           ></path>
         </svg>
         <input
